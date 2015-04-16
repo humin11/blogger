744e6c2769cc99498bc7350a543fda95dc49d88b
Hu Min <minispring1013@gmail.com>
2015-04-17 00:13:59 +0800

    init.

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mybloggerApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
