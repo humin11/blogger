c4f99985e3c39ee80a63b2050cb65fd990348b60
Hu Min <minispring1013@gmail.com>
2015-04-17 16:23:41 +0800

    add blogs & messages

--- client/app/blog/blog.controller.js
+++ client/app/blog/blog.controller.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module('mybloggerApp')
-  .controller('BlogCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('BlogCtrl', function ($scope, $http, socket) {
+    $scope.blogs = [];
+
+    $http.get('/api/blogs').success(function(awesomeThings) {
+      console.log(awesomeThings);
+      $scope.blogs = awesomeThings;
+      socket.syncUpdates('blog', $scope.blogs);
+    });
+
+    $scope.addBlog = function() {
+      if($scope.title === '') {
+        return;
+      }
+      $http.post('/api/blogs', { title: $scope.title, create_at: new Date(), content: $scope.content });
+      $scope.title = '';
+      $scope.content = '';
+    };
+
+    $scope.deleteBlog = function(blog) {
+      $http.delete('/api/blogs/' + blog._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('blog');
+    });
   });
